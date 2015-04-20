4d1953449ac161141d8a0610f57500b5ee43b284
DayoOliyide <dayo.oliyide@gmail.com>
2015-04-20 16:52:00 +0100

    Added Spacemacs config

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,4 +1,7 @@
 {:user
  {:plugins [[lein-bin "0.3.4"]
-            [lein-midje "3.1.3"]]
-  :dependencies []}}
+            [lein-midje "3.1.3"]
+            [cider/cider-nrepl "0.9.0-SNAPSHOT"]
+            [refactor-nrepl "1.0.1"]]
+  :dependencies [[alembic "0.3.2"]
+                 [org.clojure/tools.nrepl "0.2.7"]]}}
