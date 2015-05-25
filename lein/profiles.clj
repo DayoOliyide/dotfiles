94ffd2b5954dfc5ace83860723050a3423878384
DayoOliyide <dayo.oliyide@gmail.com>
2015-05-25 23:16:06 +0100

    Updated refactor-nrepl

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -2,7 +2,7 @@
  {:plugins [[lein-bin "0.3.4"]
             [lein-midje "3.1.3"]
             [cider/cider-nrepl "0.9.0-SNAPSHOT"]    ;;spacemacs cider
-            [refactor-nrepl "1.0.1"]]               ;;spacemacs cider
+            [refactor-nrepl "1.0.5"]]               ;;spacemacs cider
   :dependencies [[alembic "0.3.2"]                  ;;spacemacs cider
                  [org.clojure/tools.nrepl "0.2.7"]  ;;spacemacs cider
                  [im.chit/vinyasa "0.3.4"]
